61cff61a1ec67b29fbc2870e9ed62b923e6faacd
flashmecn <flashme@live.cn>
2019-10-09 15:56:52 +0800

    V2

---
+++ melist多级联动下拉菜单/json/all.js
@@ -0,0 +1,54 @@
+{
+	"infor":[
+        {"id":"01", "name":"danwei", "title":"赤峰市森林公安局",
+	        "link":[
+	        {"id":"01", "name":"bangong", "title":"办公室",
+		        "link":[
+		        {"id":"01", "name":"ren", "title":"张局"},
+		        {"id":"02", "name":"ren", "title":"李处"},
+		        {"id":"03", "name":"ren", "title":"王主任"},
+		        {"id":"04", "name":"ren", "title":"刘厅"}
+		        ]
+	    	},
+	        {"id":"02", "name":"bangong", "title":"警卫处",
+		        "link":[
+		        {"id":"01", "name":"ren", "title":"第二个"},
+		        {"id":"02", "name":"ren", "title":"李处"},
+		        {"id":"03", "name":"ren", "title":"王主任"},
+		        {"id":"04", "name":"ren", "title":"刘厅"}
+		        ]
+	    	},
+	        {"id":"03", "name":"bangong", "title":"局领导"},
+	        {"id":"04", "name":"bangong", "title":"会议室"},
+	        {"id":"05", "name":"bangong", "title":"监察科"}
+	        ]
+    	},
+        {"id":"02", "name":"danwei", "title":"自治区森林公安局",
+	        "link":[
+	        {"id":"01", "name":"bangong", "title":"办公室2",
+		        "link":[
+		        {"id":"01", "name":"ren", "title":"张局"},
+		        {"id":"02", "name":"ren", "title":"李处"},
+		        {"id":"03", "name":"ren", "title":"王主任"},
+		        {"id":"04", "name":"ren", "title":"刘厅"}
+		        ]
+	    	},
+	        {"id":"02", "name":"bangong", "title":"警卫处2",
+		        "link":[
+		        {"id":"01", "name":"ren", "title":"第二个"},
+		        {"id":"02", "name":"ren", "title":"李处"},
+		        {"id":"03", "name":"ren", "title":"王主任"},
+		        {"id":"04", "name":"ren", "title":"刘厅"}
+		        ]
+	    	},
+	        {"id":"03", "name":"bangong", "title":"局领导2"},
+	        {"id":"04", "name":"bangong", "title":"会议室2"},
+	        {"id":"05", "name":"bangong", "title":"监察科2"}
+	        ]
+	    },
+        {"id":"03", "name":"danwei", "title":"巴林左旗森林公安局"},
+        {"id":"04", "name":"danwei", "title":"松山区"},
+        {"id":"05", "name":"danwei", "title":"红山区"},
+        {"id":"06", "name":"danwei", "title":"赤峰市林业局"}
+    ]
+}
